d35a31d9850b54a5eec375fba0b81b3c79e676a3
Harshsharma916 <harsh.sharma@flamapp.com>
2022-02-04 02:03:45 +0530

    commented in homepage

--- routes/homepage.js
+++ routes/homepage.js
@@ -6,26 +6,26 @@ var router = express.Router();
 const client = new MongoClient(connectionString, { useUnifiedTopology: true });
 
 router.get("/", async function (req, res) {
-  await client.connect();
-  const db = await client.db("firstdb");
-  const collection = db.collection("flights");
+  // await client.connect();
+  // const db = await client.db("firstdb");
+  // const collection = db.collection("flights");
 
-  let query = req.query;
-  console.log(query.sort);
-  let flightArr = {};
-  if (query.sort == "low") {
-    flightArr = await collection
-      .aggregate([{ $sort: { price: 1 } }, { $project: { _id: 0 } }])
-      .toArray();
-  } else if (query.sort == "high") {
-    flightArr = await collection
-      .aggregate([{ $sort: { price: -1 } }, { $project: { _id: 0 } }])
-      .toArray();
-  } else {
-    flightArr = await collection.find({}, { _id: 0 }).toArray();
-  }
-  console.log(flightArr);
-  res.json(flightArr);
+  // let query = req.query;
+  // console.log(query.sort);
+  // let flightArr = {};
+  // if (query.sort == "low") {
+  //   flightArr = await collection
+  //     .aggregate([{ $sort: { price: 1 } }, { $project: { _id: 0 } }])
+  //     .toArray();
+  // } else if (query.sort == "high") {
+  //   flightArr = await collection
+  //     .aggregate([{ $sort: { price: -1 } }, { $project: { _id: 0 } }])
+  //     .toArray();
+  // } else {
+  //   flightArr = await collection.find({}, { _id: 0 }).toArray();
+  // }
+  // console.log(flightArr);
+  // res.json(flightArr);
   res.send("hello world");
 });
 
